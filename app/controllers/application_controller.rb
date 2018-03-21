298293ae1403c36bfc73ecdf40497de37d2095cf
Chris Cardle <cdcardle@gmail.com>
2018-03-21 13:39:19 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,12 +24,12 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  # get '/posts/:id' do
-  #   @post = Post.find(params[:id])
-  #
-  #   erb :show
-  # end
-  #
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+
+    erb :show
+  end
+
   # get '/posts/:id/edit' do
   #   @post = Post.find(params[:id])
   #
